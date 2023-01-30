8c63ab98d98efb7926224b84bd768dac221121ca
Bao Huynh <bao_huynh@epam.com>
2023-01-30 11:36:18 +0700

    Initial commit

---
+++ river_widget/__brick__/{{name.snakeCase()}}.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:core_riverpod/core_riverpod.dart';
+
+class {{name.pascalCase()}} extends ConsumerWidget {
+  const {{name.pascalCase()}}({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+  return const Scaffold();
+  }
+}
---
+++ view_model/__brick__/{{name.snakeCase()}}_view_model.dart
@@ -0,0 +1,20 @@
+import 'package:core_di/di.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part '{{mame.snakeCase()}}_view_model.freezed.dart';
+
+final {{mame.camelCase()}}ViewModel = StateNotifierProvider.autoDispose<{{name.pascalCase()}}ViewModel, {{name.pascalCase()}}UiState>((ref) => inject());
+
+class {{name.pascalCase()}}ViewModel extends StateNotifier<{{name.pascalCase()}}UiState> {
+  {{name.pascalCase()}}ViewModel() : super({{name.pascalCase()}}UiState.initial());
+
+  
+}
+
+@freezed
+class {{name.pascalCase()}}UiState with _${{name.pascalCase()}}UiState {
+  const factory {{name.pascalCase()}}UiState() = _HomeUiState;
+
+  factory {{name.pascalCase()}}UiState.initial() => const {{name.pascalCase()}}UiState();
+}
