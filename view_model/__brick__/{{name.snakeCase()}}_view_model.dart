7db6e689497d012938d69d4f161f5e25441f6f30
Bao Huynh <bao_huynh@epam.com>
2023-01-30 11:41:32 +0700

    Fix name

--- view_model/__brick__/{{name.snakeCase()}}_view_model.dart
+++ view_model/__brick__/{{name.snakeCase()}}_view_model.dart
@@ -2,9 +2,9 @@ import 'package:core_di/di.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 
-part '{{mame.snakeCase()}}_view_model.freezed.dart';
+part '{{name.snakeCase()}}_view_model.freezed.dart';
 
-final {{mame.camelCase()}}ViewModel = StateNotifierProvider.autoDispose<{{name.pascalCase()}}ViewModel, {{name.pascalCase()}}UiState>((ref) => inject());
+final {{name.camelCase()}}ViewModel = StateNotifierProvider.autoDispose<{{name.pascalCase()}}ViewModel, {{name.pascalCase()}}UiState>((ref) => inject());
 
 class {{name.pascalCase()}}ViewModel extends StateNotifier<{{name.pascalCase()}}UiState> {
   {{name.pascalCase()}}ViewModel() : super({{name.pascalCase()}}UiState.initial());
